d0c65766d1e26dba5757c543a5c7e5920c4608f7
Diego Novais <contato@diegonovais.com.br>
2020-09-12 23:35:50 -0300

    Relations and factories created

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,4 @@
 class Question < ApplicationRecord
   belongs_to :discipline
+  has_many :question_accesses
 end
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :question do
-    discipline { nil }
-    statement { "MyText" }
-    text { "MyText" }
-    answer { "MyString" }
-    daily_access { 1 }
+    discipline
+    statement { Faker::Lorem.sentences(number: 1) }
+    text { Faker::Lorem.paragraphs }
+    answer { ('A'..'E').to_a.sample }
+    daily_access { Faker::Number.number(digits: 2) }
   end
 end
