97281d6afa3ecf6bb25870a6ed2734e6ba566f22
Diego Novais <contato@diegonovais.com.br>
2020-09-30 01:59:49 -0300

    Creating most accessed questions query object

--- spec/factories/question_accesses.rb
+++ spec/factories/question_accesses.rb
@@ -1,7 +1,19 @@
 FactoryBot.define do
   factory :question_access do
     question
-    date { Faker::Date.between(from: 10.days.ago, to: Date.today) }
+    date { Faker::Date.between(from: 60.days.ago, to: Date.today) }
     times_accessed { Faker::Number.number(digits: 5) }
+
+    trait :this_week do
+      date { Faker::Date.between(from: DateTime.now.beginning_of_week, to: Date.today) }  
+    end
+
+    trait :this_month do
+      date { Faker::Date.between(from: DateTime.now.beginning_of_month, to: Date.today) }  
+    end
+
+    trait :this_year do
+      date { Faker::Date.between(from: DateTime.now.beginning_of_year, to: Date.today) }  
+    end
   end
 end
