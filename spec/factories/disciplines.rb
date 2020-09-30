a72adeca53a809499c04c6ca5fcf2ea1fd1a93ce
Diego Novais <contato@diegonovais.com.br>
2020-09-30 01:57:14 -0300

    Creating hotter question query object

--- spec/factories/disciplines.rb
+++ spec/factories/disciplines.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :discipline do
-    name { ["português", "direito civil", "legislação federal", "direito constitucional", "literatura", "estatística", "geografia", "inglês", "raciocínio lógico", "matemática"].sample }
+    name { ["português", "direito civil", "legislação federal", 
+            "direito constitucional", "literatura", "estatística", 
+            "geografia", "inglês", "raciocínio lógico", "matemática"].sample }
   end
 end
